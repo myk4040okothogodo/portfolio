81498cba787df5ae2b5e002d6e8d50583fb21029
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-30 21:25:04 +0300

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -2,7 +2,7 @@ import NextLink from 'next/link';
 import {Heading, Box, Image, Link, Badge} from '@chakra-ui/react';
 import { ChevronRightIcon} from '@chakra-ui/icons'
 
-export const Title = ({ chiildren }) => (
+export const Title = ({ children }) => (
   <Box>
     <NextLink href="/works">
       <Link>Works</Link>
